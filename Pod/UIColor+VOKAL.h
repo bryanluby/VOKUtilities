f4fb83663fa2d221d9513fe2d5ecf97c9c654e74
Isaac Greenspan <isaac.greenspan@vokalinteractive.com>
2014-11-10 16:27:51 -0600

    Adding UIColor methods to take hex strings and method to get hex string of a UIColor.

--- Pod/UIColor+VOKAL.h
+++ Pod/UIColor+VOKAL.h
@@ -29,4 +29,32 @@
  */
 + (instancetype)vok_colorWithHexInt:(NSUInteger)hexColor;
 
+/**
+ *  Generate a UIColor * based on a color expressed as a hexadecimal string and an alpha component.
+ *
+ *  @param hexString The hexadecimal color (i.e., web representation), as a string; can be full six-digit, 
+ *                   three-digit shorthand for repeated digits (i.e., XYZ means XXYYZZ), or one- or two-digit
+ *                   for grayscale (i.e., X means XXXXXX and XY means XYXYXY)
+ *  @param alpha     The alpha component, expressed as a floating-point number between 0 (transparent) and 1 (solid)
+ *
+ *  @return The generated color
+ */
++ (instancetype)vok_colorWithHexString:(NSString *)hexString alpha:(CGFloat)alpha;
+
+/**
+ *  Generate a UIColor * based on a color expressed as a hexadecimal string, with alpha 1.0 (solid).
+ *
+ *  @param hexString The hexadecimal color (i.e., web representation), as a string; can be full six-digit,
+ *                   three-digit shorthand for repeated digits (i.e., XYZ means XXYYZZ), or one- or two-digit
+ *                   for grayscale (i.e., X means XXXXXX and XY means XYXYXY)
+ *
+ *  @return The generated color
+ */
++ (instancetype)vok_colorWithHexString:(NSString *)hexString;
+
+/**
+ *  @return The six-digit hexadecimal string representation of the receiver with leading # (@"#RRGGBB")
+ */
+- (NSString *)vok_hexString;
+
 @end
--- Pod/UIColor+VOKAL.m
+++ Pod/UIColor+VOKAL.m
@@ -23,4 +23,56 @@
     return [self vok_colorWithHexInt:hexColor alpha:1.0f];
 }
 
++ (instancetype)vok_colorWithHexString:(NSString *)hexString alpha:(CGFloat)alpha
+{
+    // Remove any non-hexadecimal characters from the ends of the input string.
+    NSCharacterSet *nonHexadecimalCharacterSet = [[NSCharacterSet characterSetWithCharactersInString:@"0123456789ABCDEFabcdef"] invertedSet];
+    hexString = [hexString stringByTrimmingCharactersInSet:nonHexadecimalCharacterSet];
+    
+    unsigned int rgbValue = 0;
+    
+    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    [scanner scanHexInt:&rgbValue];
+    NSUInteger length = scanner.scanLocation;
+    switch (length) {
+        case 1:
+            // If we only got a single hex digit, then it's shorthand for the equivalent with that digit repeated in all 6 places.
+            return [self vok_colorWithHexInt:rgbValue * 0x111111 alpha:alpha];
+            
+        case 2:
+            // If we got two hex digits, then it's shorthand for those same two digits used for each of the 3 color components.
+            return [self vok_colorWithHexInt:rgbValue * 0x010101 alpha:alpha];
+            
+        case 3:
+            // If we got three hex digits, then it's shorthand for using the first digit repeated twice as the red,
+            // the second digit repeated twice as the green, and the third digit repeated twice as the blue.
+            return [self vok_colorWithHexInt:((rgbValue >> 8) * 0x110000
+                                              + ((rgbValue >> 4) & 0xF) * 0x001100
+                                              + (rgbValue & 0xF) * 0x000011)
+                                       alpha:alpha];
+            
+        default:
+            // Otherwise, assume it's a full 6-digit color.
+            return [self vok_colorWithHexInt:rgbValue alpha:alpha];
+    }
+}
+
++ (instancetype)vok_colorWithHexString:(NSString *)hexString
+{
+    return [self vok_colorWithHexString:hexString alpha:1.0f];
+}
+
+- (NSString *)vok_hexString
+{
+    CGFloat red;
+    CGFloat green;
+    CGFloat blue;
+    CGFloat alpha;
+    
+    [self getRed:&red green:&green blue:&blue alpha:&alpha];
+    
+    return [NSString stringWithFormat:@"#%02X%02X%02X",
+            (unsigned int)(red * 255.0), (unsigned int)(green * 255.0), (unsigned int)(blue * 255.0)];
+}
+
 @end
